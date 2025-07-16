604dfdce11d141ac2cdc661724835783ef0fce0f
Gabriel Ross <gabe-design@XPS-16-0506.localdomain>
2025-07-16 13:53:55 -0700

    Tasks

---
+++ app/models/task.py
@@ -0,0 +1,34 @@
+# app/models/task.py
+
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255), nullable=False)
+    is_completed = db.Column(db.Boolean, default=False)
+    note_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('notes.id')), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    note = db.relationship('Note', back_populates='tasks')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'content': self.content,
+            'is_completed': self.is_completed,
+            'note_id': self.note_id,
+            'created_at': self.created_at.isoformat() if self.created_at else None,
+            'updated_at': self.updated_at.isoformat() if self.updated_at else None,
+        }
+    
+    return data
+
+    #remember to add to notes model 
+    # tasks = db.relationship('Task', back_populates='note', cascade='all, delete-orphan')
