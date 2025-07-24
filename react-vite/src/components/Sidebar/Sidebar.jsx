f5d01126e2dfa420341b9456340253d2a28c5183
Gabe-design <gabrielaross8@gmail.com>
2025-07-23 22:14:06 -0700

    Finished components

---
+++ react-vite/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,21 @@
+// // react-vite/src/compenets/Sidebar.jsx
+
+// // This is a sidebar to match the wireframes
+// import { NavLink } from "react-router-dom"
+// import "./sidebar.css"
+
+// export default function Sidebar() {
+//     return (
+//         <div className="sidebar">
+//             <nav>
+//                 <ul>
+//                     <li><NavLink to="/">Home</NavLink></li>
+//                     <li><NavLink to="/notes">Notes</NavLink></li>
+//                     <li><NavLink to="/tasks">Tasks</NavLink></li>
+//                     <li><NavLink to="/notebooks">Notebooks</NavLink></li>
+//                     <li><NavLink to="/tags">Tags</NavLink></li>
+//                 </ul>
+//             </nav>
+//         </div>
+//     );
+// }
