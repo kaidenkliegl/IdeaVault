7b9a97c904b4ccce6e4a9382e8dba0ebb5bff376
Gabe-design <gabrielaross8@gmail.com>
2025-07-24 18:44:37 -0700

    Finished tasks components and redux

---
+++ react-vite/src/components/Tasks/TaskForm.jsx
@@ -0,0 +1,94 @@
+// react-vite/src/components/Tasks/TaskList.jsx
+
+import { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { thunkCreateTask, thunkUpdateTask, thunkFetchTasks } from '../../redux/tasks';
+
+// This will be the TaskForm function
+function TaskForm() {
+    const dispatch = useDispatch();
+
+    // These are the local frorm state
+    const [content, setContent] = useState('');
+    const [noteId, setNoteId] = useState('');
+    const [errors, setErrors] = useState({});
+    // 
+    const {taskId} = useParams();
+    const navigate = useNavigate();
+    // this will determine if in edit mode
+    const isEditing = !!taskId;
+    const task = useSelector((statet) => statet.tasks[taskId]);
+
+    // This will load task data if editing
+    useEffect(() => {
+        if (!isEditing && !task) {
+            dispatch(thunkFetchTasks());
+
+        } else if (isEditing) {
+            setContent(task?.content);
+            setNoteId(task?.note_id);
+        }
+
+    }, [dispatch, isEditing, task]);
+
+    // This will handle form submission
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        let res;
+
+        if (isEditing) {
+            res = await dispatch(thunkUpdateTask(taskId, {content}));
+
+        } else {
+            res = await dispatch(thunkCreateTask(noteId, {content}));
+
+        }
+
+        // And this will handle validation errors
+        if (res?.errors) {
+            setErrors(res.errors);
+
+        } else {
+            navigate('/tasks');
+        }
+    };
+
+    return (
+        <div className='task-form-container'>
+            {/* Page title */}
+            <h1>{isEditing ? 'Edit' : 'New'} Task </h1>
+            {/* Task form */}
+            <form onSubmit={handleSubmit} className='task-form'>
+                <label>
+                    Note ID
+                    <input
+                        type='number'
+                        value={noteId}
+                        onChange={(e) => setNoteId(e.target.value)}
+                        required
+                        disabled={isEditing}
+
+                    />
+                </label>
+                <label>
+                    Content
+                    <input
+                        type='text'
+                        value={context}
+                        onChange={(e) => setContent(e.target.value)}
+                        required
+                    />
+                </label>
+                {/* Show validation errors */}
+                {errors.content && <p className='error-text'>{errors.content}</p>}
+                {/* Submit button */}
+                <button type='submit' className='submit-button'>
+                    {isEditing ? 'Update' : 'Create'}
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default TaskForm;
---
+++ react-vite/src/redux/tasks.js
@@ -0,0 +1,124 @@
+// react-vote/src/components/redux/tasks.js
+
+// these are the action types
+const SET_TASKS = 'tasks/setTasks';
+const ADD_TASK = 'tasks/addTask';
+const UPDATE_TASK = 'tasks/updateTask';
+const DELETE_TASK = 'tasks/deleteTask';
+
+// These ar going to be the action creators
+const setTasks = (tasks) => ({
+    type: SET_TASKS,
+    payload: tasks
+})
+
+const addTask = (task) => ({
+    type: ADD_TASK,
+    payload: task
+})
+
+const updateTask = (task) => ({
+    type: UPDATE_TASK,
+    payload: task
+})
+
+const deleteTask = (taskId) => ({
+    type: DELETE_TASK,
+    payload: task
+})
+
+// These are going to be the thunbks for tasks
+export const thunkFetchTasks = () => async (dispatch) => {
+    const res = await fetch('/api/tasks/');
+    if (res.ok) {
+        const data = res.json();
+        dispatch(setTasks(data.tasks));
+
+    }
+};
+
+export const thunkCreateTask = (noteId, taskdata) => async (dispatch) => {
+    const res = await fetch(`/api/tasks/notes/${noteId}/tasks`, {method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(taskData)})
+
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(addTask(data.task));
+        return data;
+
+    } else if (res.status < 500) {
+        const data = await res.json();
+        return data; 
+    } else {
+        return { server: 'Something went wrong. Please try again'};
+
+    }
+};
+
+export const thunkUpdateTask = (taskId, taskData) => async (dispatch) => {
+    const res = await fetch(`api/tasks/${taskId}`, { method: 'PUT', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(taskData)});
+
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(updateTask(data.task));
+        return data;
+
+    } else if (res.status < 500) {
+        const data = await res.json();
+        return data; 
+    } else {
+        return { server: 'Something went wrong. Please try again'};
+
+    }
+};
+
+export const thunkDeleteTask = (taskId) => async (dispatch) => {
+    const res = await fetch(`/api/tasks/${taskId}`, { method: 'DELETE' });
+
+    if (res.ok) {
+
+        dispatch(deleteTask(taskId));
+
+
+    } else if (res.status < 500) {
+        const data = await res.json();
+        return data; 
+    } else {
+        return { server: 'Something went wrong. Please try again'};
+
+    }
+};
+
+// This is going to be the reducer fior tasks
+const initialState = {};
+
+function tasksReducer(state = initialState, action) {
+    switch (action.type) {
+        case SET_TASKS: {
+            const newState = {};
+            action.payload.forEach(task => {
+                newState[task.id] = task;
+            });
+            return newState
+
+        }
+
+        case ADD_TASK:
+        case UPDATE_TASK:
+            return {
+                ...state,
+                [action.payload.id]: action.payload
+            };
+        
+        case DELETE_TASK: {
+            const newState = {...state};
+            delete newState[action.payload];
+            return newState;
+        }
+
+        default:
+            return state
+    };
+
+}
+
+export default tasksReducer;
